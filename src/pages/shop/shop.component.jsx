469b1408cf820d486b8b6f6bfa992a961fb5b6b2
rodrigo <rodrigolemes@live.com>
2020-02-28 15:19:27 -0300

    Updated the app with a shop page with collection components

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import './shop.styles.scss';
+
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      // This is just placeholder data not related to pet shops at all, to be replaced later.
+      collections: SHOP_DATA
+    }
+  }
+  
+  render() {
+    const { collections } = this.state;
+
+    return (
+      <div className="shop-page">
+        {
+          collections.map(({id, ...otherCollectionProps}) => (
+            <CollectionPreview key={id} {...otherCollectionProps} />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+export default ShopPage;
