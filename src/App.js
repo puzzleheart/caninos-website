fbf60c2bff2f9c45ac605d69caf66cd47b963f2f
rodrigo <rodrigolemes@live.com>
2020-02-28 13:39:50 -0300

    updated navigation adding react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const RoupasPage = () => (
+  <div>
+    <h1>ROUPAS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <HomePage/>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}></Route>
+        <Route exact path='/shop/roupas' component={RoupasPage}></Route>
+      </Switch>
+    </div>
   );
 }
 
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ export default class Directory extends Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({title, id, imageUrl, size}) => (
-          <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({id, ...otherSectionProps}) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
