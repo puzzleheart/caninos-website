c682eb06c509f2e256f87bc99a5c18ff50ff578c
rodrigo <rodrigolemes@live.com>
2020-02-29 00:41:28 -0300

    Added sign in components and reusable, dynamic button and input

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+  <div>
+    <div className="sign-in-and-sign-up">
+      <SignIn/>
+    </div>
+  </div>
+);
+
+export default SignInAndSignUpPage;
