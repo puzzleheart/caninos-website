47793eac46af3572ff341b320db4b11af79cc398
rodrigo lemes <rodrigolemes@live.com>
2020-03-02 23:38:58 -0300

    updated cart reducer, actions to add items to cart, updated collections to use redux

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,17 +3,18 @@ import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
-const CollectionPreview = ({title, items}) => (
-  <div className="collection-preview">
-    <h1 className="title">{title}</h1>
-    <div className="preview">
-      {
-        items.filter((item, index) => index < 4).map(({id, ...otherItemProps}) => (
-          <CollectionItem key={id} {...otherItemProps} />
-        )) 
-      }
+// receives data from the shop page
+const CollectionPreview = ({ title, items }) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title}</h1>
+    <div className='preview'>
+      {items
+        .filter((item, index) => index < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
   </div>
-)
+);
 
 export default CollectionPreview;
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,19 @@
+// Remember: We must return a new object in order for react to update the dom 
+// (can't just return the same cart items but with a changed property)
+
+// Takes in a cartItems state + an item payload and returns the new state
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
