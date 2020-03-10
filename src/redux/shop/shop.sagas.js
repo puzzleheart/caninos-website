893a71786f97ca802bac48ee1872ac16ec992eed
rodrigo lemes <rodrigolemes@live.com>
2020-03-09 23:20:01 -0300

    Added redux-saga, created root and fetchCollectionsAsync saga

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,40 @@
+// takeEvery: listens for every action of a specific type that we pass to it
+
+import { takeEvery, call, put } from 'redux-saga/effects';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure
+} from './shop.actions';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+  yield console.log('fetchCollectionsAsync was fired');
+  try {
+    const collectionRef = firestore.collection('collections');
+    const snapshot = yield collectionRef.get();
+
+    // All that call does is invoke a function; it's safer to use it in case the function takes long to execute
+    // Adding more yields wherever we can makes it easier to test
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,22 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk'; 
+import createSagaMiddleware from 'redux-saga';
 
 import rootReducer from './root-reducer';
+import rootSaga from './root-saga';
 
-// While redux-thunk middleware is enabled, any time you attemp to dispatch a function instead of an object, 
-// the middleware will call that function with dispatch method itself as the first argument
-const middlewares = [thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
   middlewares.push(logger);
 }
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
